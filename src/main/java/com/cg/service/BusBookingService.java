314f827767bdafcd28339bcbe6c919c0c956e48d
Himanshu-ABES <himanshu.22b1541059@abes.ac.in>
2026-04-16 15:11:50 +0530

    service interface created

--- src/main/java/com/cg/service/BusBookingService.java
+++ src/main/java/com/cg/service/BusBookingService.java
@@ -1,5 +1,11 @@
 package com.cg.service;
 
-public interface BusBookingService {
+import com.cg.entity.BusBooking;
+import com.cg.entity.RouteSchedule;
+import java.util.List;
 
+public interface BusBookingService {
+    RouteSchedule createSchedule(RouteSchedule schedule);
+    BusBooking bookTicket(Long customerId, Long scheduleId, BusBooking booking);
+    List<BusBooking> getBookingsByCustomer(Long customerId);
 }

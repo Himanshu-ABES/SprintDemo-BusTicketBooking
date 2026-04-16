9f6721996d48f15bac5811906f4b24f0e6345d35
Singhrishabh1810 <rishabhsingh1810@gmail.com>
2026-04-16 15:06:22 +0530

    entities created

--- src/main/java/com/cg/entity/Customer.java
+++ src/main/java/com/cg/entity/Customer.java
@@ -1,5 +1,35 @@
 package com.cg.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long cust_id;
+	private String cust_name;
+	private long phone_no;
+	public long getCust_id() {
+		return cust_id;
+	}
+	public void setCust_id(long cust_id) {
+		this.cust_id = cust_id;
+	}
+	public String getCust_name() {
+		return cust_name;
+	}
+	public void setCust_name(String cust_name) {
+		this.cust_name = cust_name;
+	}
+	public long getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(long phone_no) {
+		this.phone_no = phone_no;
+	}
+	
 
 }
